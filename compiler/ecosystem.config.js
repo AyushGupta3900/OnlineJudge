52b5e831b96f6453ccb5ce653fea3df46b80a57f
Ayush Gupta <12211028a@gmail.com>
2025-07-07 16:58:02 +0530

    dockerization done

---
+++ compiler/ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: "compiler-server",
+      script: "server.js",
+      instances: 1,
+      exec_mode: "fork",
+    },
+    {
+      name: "worker",
+      script: "worker.js",
+      instances: 3,
+      exec_mode: "fork",
+    }
+  ]
+}
